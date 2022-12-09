942269cdd8643bbb7a6c2ce51e555baa54b4f148
Steven Stavrakis <s.a.stavrakis@gmail.com>
2022-12-09 11:24:21 -0500

    big commit

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,12 @@
+import { day7 } from './day-7/index';
 import { day2 } from './day-2/index';
 import { findMostCalories } from "./day-1";
-import {day3} from "./day-3"
-
+import { day3 } from "./day-3"
+import { day4 } from './day-4';
+import { day5 } from './day-5'
+import { day6 } from './day-6';
+import { day8 } from './day-8'
+import { day9 } from './day-9';
 // day one
 
 const addResult = async (func: () => any, day: number) => {
@@ -10,13 +15,19 @@ const addResult = async (func: () => any, day: number) => {
     resultElement.innerHTML = `Day ${day} result: ${result}`;
     document.body.appendChild(resultElement);
 }
-
+/*
 addResult(findMostCalories, 1);
 addResult(day2, 2)
 addResult(day3, 3)
+addResult(day4, 4)
+addResult(day5, 5)
+addResult(day6, 6)*/
+// addResult(day7, 7)
+//addResult(day8, 8)
+addResult(day9, 9)
 const title = document.createElement("h1");
 title.innerText = "Hello World";
 document.body.appendChild(title);
 
 
-export {};
+export { };
